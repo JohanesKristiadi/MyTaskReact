ca78727b22bfac91b16dc098c3ba5ecfcf86b269
JohanesKristiadi <johanes.kristiadi@student.umn.ac.id>
2023-02-09 21:48:04 +0700

    create TodoItem style

--- src/componnents/TodoItem.js
+++ src/componnents/TodoItem.js
@@ -1,11 +1,27 @@
 import React from "react";
+import Button from "./Button"
 
 const TodoItem = () =>{
     return(
-        <div>
-            todo item
+        <div style={todoItem}>
+            <p>Title Todo</p>
+            <div>
+                <Button/>
+                <Button/>
+            </div>
         </div>
     )
 }
 
 export default TodoItem;
+
+const todoItem = {
+    background :"#6096B4",
+    color : "#ffff",
+    display : "flex",
+    alignItems : "center",
+    height : "3rem",
+    padding : "0 1rem",
+    justifyContent : "space-between",
+    margin : "0.5rem 0"
+}
