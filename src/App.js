1fdfad2a8f86bc1167e6d7543b28eb1653fcd33e
JohanesKristiadi <johanes.kristiadi@student.umn.ac.id>
2023-02-09 21:22:57 +0700

    create css for app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import logo from './logo.svg'
+import FormInput from './componnents/FormInput';
+import Button from './componnents/Button';
+import TodoItem from './componnents/TodoItem';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
+    <div className="app">
+      <div className='logo'>
+        <img src={logo} alt='logo'></img>
+        <h3>Task List</h3>
+      </div>
+      <div className='list'>
+        <TodoItem/>
+      </div>
+      <div className='input-form'>
+        <FormInput/>
+      </div>
+
+
     </div>
   );
 }
