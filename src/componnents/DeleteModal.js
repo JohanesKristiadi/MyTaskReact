4a707de4e8baaaa4ec008ae247e3e23d350c52f1
JohanesKristiadi <johanes.kristiadi@student.umn.ac.id>
2023-02-12 21:31:45 +0700

    create delete modal

--- src/componnents/DeleteModal.js
+++ src/componnents/DeleteModal.js
@@ -1,16 +1,19 @@
 import React from "react";
-import "../styles/DeleteModal.css"
+import "../styles/DeleteModal.css";
 import Button from "./Button";
 
-class DeleteModal extends React.Component{
-    render(){
-        return(
-            <div>
-                <h3>Apakah Anda Ingin menghapus data??</h3>
-                <Button text="Iya"  variant="success"/>
-                <Button text="Tidak Jadi" variant="warning"/>
+class DeleteModal extends React.Component {
+    render() {
+        
+        return (
+            <div className="modal-container">
+                <div className="modal-box">
+                    <h3>Apakah Anda Ingin menghapus data??</h3>
+                    <Button text="Iya" variant="success" />
+                    <Button text="Tidak Jadi" variant="warning"/>
+                </div>
             </div>
-        )
+        );
     }
 }
 
